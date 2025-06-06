13de261f2f1c61995c4fe95b4ee3c2e05cf5286f
vanessa hasckel <vanessahasckel00@hotmail.com>
2025-06-06 20:47:39 -0300

    fix: add Suspense boundary to Search component

--- app/collection-points/page.tsx
+++ app/collection-points/page.tsx
@@ -1,10 +1,13 @@
 import { CollectionPointsList } from "@/features/collection-points/components/list";
 import { Search } from "@/features/collection-points/components/search";
+import { Suspense } from "react";
 
 export default function CollectionPoints() {
     return (
         <main>
-            <Search />
+            <Suspense fallback={<div>Carregando...</div>}>
+                <Search />
+            </Suspense>
             <CollectionPointsList />
         </main>
     );
